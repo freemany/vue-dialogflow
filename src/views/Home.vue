9903710ec46e5bdd39595d15f4f1b29ecaaf8e65
Paul Aeria <pm.aeria@gmail.com>
2019-02-04 11:18:02 +0800

    More changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>My First Name Is {{ firstName }}</h2>
+    <h2>My Last Name Is {{ lastName }}</h2>
+    <h2>My Full Name Is {{ fullName }}</h2>
+    <button @click="onClick">Fetch Name</button>
+
+    <button @click="addCar">Add Car</button>
+    <button @click="addBicycle">Add Bicycle</button>
+    <div class="container">
+      <div class="list-containers"> 
+        <h3>state.vehicles</h3>
+        <ul>
+          <li v-for="(vehicle, index) in vehicles" :key="index">
+            {{vehicle.name}}
+          </li>
+        </ul>
+      </div>
+      <div class="list-containers"> 
+        <h3>getters.cars</h3>
+        <ul>
+          <li v-for="(car, index) in cars" :key="index">
+            {{car.name}}
+          </li>
+        </ul>
+      </div>
+      <div class="list-containers"> 
+      <h3>getters.bicycles</h3>
+        <ul>
+          <li v-for="(bicycle, index) in bicycles" :key="index">
+            {{bicycle.name}}
+          </li>
+        </ul>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState, mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  methods: {
+    onClick() {
+      this.fetchName();
+    },
+    addBicycle() {
+      this.addVehicle({ vehicle: {
+        name: 'new bicycle',
+        type: 'Bicycle'
+      }});
+    },
+    addCar() {
+      this.addVehicle({ vehicle: {
+        name: 'new car',
+        type: 'Car'
+      }});
+    },
+    ...mapActions(['addVehicle','fetchName'])
+  },
+  computed: {
+    ...mapState(['firstName','lastName', 'vehicles']),
+    ...mapGetters(['fullName','cars','bicycles'])
   }
 }
 </script>
+<style>
+  .container {
+    margin: 40px auto;
+    width: 500px;
+    display: flex;
+    justify-content: space-between;
+  }
+  ul { list-style: none; padding: 0; } 
+</style>
